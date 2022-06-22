c66ff1f1694e8be674e0bdfbb0ef144a73f997c9
guiroos <guilherme.roosr@gmail.com>
2022-06-22 15:35:25 -0300

    feat: add id to book interface

--- src/interface/IBook.ts
+++ src/interface/IBook.ts
@@ -1,6 +1,7 @@
 // https://stackoverflow.com/questions/57086672/element-implicitly-has-an-any-type-because-expression-of-type-string-cant-b
 export interface IBook {
   [key: string]: string | number | boolean,
+  id: number,
   author: string,
   country: string,
   imageLink: string,
